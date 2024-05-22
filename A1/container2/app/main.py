f9469303332025833b796099c1bdb1ea6978f7e4
Ashish Kumar Guntipalli <as589490@dal.ca~>
2024-05-22 10:02:16 -0300

    correction #2

--- A1/container2/app/main.py
+++ A1/container2/app/main.py
@@ -20,11 +20,8 @@ async def sum_product(data: InputData):
         with open(file_path, newline='') as csvfile:
             csv_reader = csv.reader(csvfile, delimiter=',')
             for row in csv_reader:
-                if len(row) != 2:
-                    return { "file": file_name, "error": "Input file not in CSV format.", "row":row}
                 if(row[0] == product):
                     sum = sum + int(row[1])
-
         return { "file": file_name, "sum": sum}
     except Exception as e:
         return {"file": file_name, "error": "Input file not in CSV format."}
