a84f8a6c7029a0be6ca3761497b1d651f0f8db52
Ashish Kumar Guntipalli <as589490@dal.ca~>
2024-05-22 08:53:01 -0300

    correction #2

--- A1/container2/app/main.py
+++ A1/container2/app/main.py
@@ -17,8 +17,13 @@ async def sum_product(data: InputData):
 
     try:
         with open(file_path, newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
-            total = sum(int(row['amount']) for row in reader if row['product'] == product)
-        return {"file": file_name, "sum": total}
+            csv_reader = csv.reader(csvfile, delimiter=',')
+            for row in csv_reader:
+                if len(row) != 2:
+                    return { "file": file_name, "error": "Input file not in CSV format."}
+                if(row[0] == product):
+                    sum = sum + int(row[1])
+
+        return { "file": file_name, "sum": sum}
     except Exception as e:
         return {"file": file_name, "error": "Input file not in CSV format."}
