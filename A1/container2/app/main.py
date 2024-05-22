cf80456bf08e30f78088e4e81185d5723a12b9d1
Ashish Kumar Guntipalli <as589490@dal.ca~>
2024-05-22 10:10:58 -0300

    correction #2

--- A1/container2/app/main.py
+++ A1/container2/app/main.py
@@ -16,12 +16,9 @@ async def sum_product(data: InputData):
 
     file_path = f"/shared_volume/{file_name}"
 
-    try:
-        with open(file_path, newline='') as csvfile:
-            csv_reader = csv.reader(csvfile, delimiter=',')
-            for row in csv_reader:
-                if(row[0] == product):
-                    sum = sum + int(row[1])
-        return { "file": file_name, "sum": sum}
-    except Exception as e:
-        return {"file": file_name, "error": "Input file not in CSV format."}
+    with open(file_path, newline='') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',')
+        for row in csv_reader:
+            if(row[0] == product):
+                sum = sum + int(row[1])
+    return { "file": file_name, "sum": sum}
