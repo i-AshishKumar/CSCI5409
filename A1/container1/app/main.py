9f5c64b5fe07c0f4f70299d532edf8f532bffc7b
Ashish Kumar Guntipalli <as589490@dal.ca~>
2024-05-22 08:41:46 -0300

    correction #2

--- A1/container1/app/main.py
+++ A1/container1/app/main.py
@@ -6,7 +6,7 @@ import os
 app = FastAPI()
 
 class InputData(BaseModel):
-    file: str | None
+    file: str
     product: str
 
 @app.post("/calculate")
@@ -19,7 +19,7 @@ async def calculate(data: InputData):
     if not os.path.exists(file_path):
         return {"file": file_name, "error": "File not found."}
 
-    if file_name == None:
+    if not file_name:
         return {"file": None, "error": "Invalid JSON input."}
 
     response = requests.post("http://container2:7000/sum", json={"file": file_name, "product": product})
--- A1/container2/app/main.py
+++ A1/container2/app/main.py
@@ -5,7 +5,7 @@ import csv
 app = FastAPI()
 
 class InputData(BaseModel):
-    file: str | None
+    file: str
     product: str
 
 @app.post("/sum")
@@ -20,14 +20,5 @@ async def sum_product(data: InputData):
             reader = csv.DictReader(csvfile)
             total = sum(int(row['amount']) for row in reader if row['product'] == product)
         return {"file": file_name, "sum": total}
-
-            # csv_reader = csv.reader(csvfile, delimiter=',')
-            # for row in csv_reader:
-            #     if len(row) != 2:
-            #         return {"file": file_name, "error": "Input file not in CSV format." }
-            #     if(row[0] == product):
-            #         sum = sum + int(row[1])
-            # return {"file": file_name, "sum": sum}
-        
     except Exception as e:
-        return {"error": "Input file not in CSV format.","file": file_name, }
+        return {"file": file_name, "error": "Input file not in CSV format."}
