10d2d28b61a644bc68cd56d12f54891a7e9c23c5
Ashish Kumar Guntipalli <as589490@dal.ca>
2024-07-13 22:56:00 -0300

    A3 initial

---
+++ A3/md5_lambda.mjs
@@ -0,0 +1,29 @@
+import { createHash } from 'crypto';
+import axios from 'axios';
+
+export const handler = async (event, context) => {
+  
+  // Calculate Hash
+  const hashValue = createHash('md5').update(event.value).digest('hex');
+  
+  // Create a JSON Payload
+  const reqBody = {
+    "banner": "B00981016",
+    "result": hashValue,
+    "arn": context.invokedFunctionArn,
+    "action": event.action,
+    "value": event.value
+  };
+  
+  // Send to Server
+  await axios.post(event.course_uri, reqBody)
+  .then(function (response) {
+    console.log("Hash sent to",event.course_uri);
+    console.log(response.data);
+  })
+  .catch(function (error) {
+   console.error("Something went wrong.");
+   console.error(error);
+  });
+  
+};
